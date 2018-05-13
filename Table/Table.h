373f2db09b128c8149fa2c5664c192c57bc69938
Starovoytov Alexey <relz0071@gmail.com>
2018-05-13 12:51:17 +0300

    Убран вывод таблицы в выходной поток

--- Table/Table.h
+++ Table/Table.h
@@ -11,12 +11,9 @@ class Table
 public:
 	void AddRow(unsigned int id, TableRow * row);
 	TableRow * GetRow(unsigned int id) const;
-	void Print(std::ostream & ostream) const;
 
 private:
 	std::unordered_map<unsigned int, TableRow *> m_rows = std::unordered_map<unsigned int, TableRow *>();
-
-	std::string SetToString(std::unordered_set<std::string> const & set) const;
 };
 
 #endif
